a9d71f0cdb0d9082658fdb8432a17187b06f9c2a
javier gimenez <javierzlafe@gmail.com>
2025-07-23 17:13:40 -0300

    estructura basica

---
+++ index.js
@@ -0,0 +1,8 @@
+import express from "express";
+const app = express();
+app.get("/", (req,res)=>{
+    res.json({message: "API REST"})
+})
+const PORT =3000
+
+app.listen(PORT, ()=> console.log(`http://localhost:${PORT}`));
